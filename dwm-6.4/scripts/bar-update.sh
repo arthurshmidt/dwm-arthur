3f0b1c5e6b8e6d5735f960ed1a85de668e3eb531
arthurshmidt <arthur@arthurshmidt.com>
2024-02-12 23:35:40 -0500

    updated status bar

--- dwm-6.4/scripts/bar-update.sh
+++ dwm-6.4/scripts/bar-update.sh
@@ -6,8 +6,9 @@ while true; do
 	# xsetroot -name "$(echo -e '\uf240')  $(cat /sys/class/power_supply/BAT0/capacity)%"
 	bat_icon=$(echo -e '\uf240')
 	bat_percent=$(cat /sys/class/power_supply/BAT0/capacity)%
+	wifi_icon=$(echo -e '\uf1eb')
 
-	xsetroot -name "$bat_icon  $bat_percent"
+	xsetroot -name " $wifi_icon  | $bat_icon  $bat_percent"
 
 	sleep 2
 done
