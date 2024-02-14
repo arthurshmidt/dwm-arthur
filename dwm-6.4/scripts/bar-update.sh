21de8e8ee99a849b2c4dfc30f1648fb26b273adc
arthurshmidt <arthur@arthurshmidt.com>
2024-02-13 21:57:17 -0500

    status bar update

--- dwm-6.4/scripts/bar-update.sh
+++ dwm-6.4/scripts/bar-update.sh
@@ -7,8 +7,10 @@ while true; do
 	bat_icon=$(echo -e '\uf240')
 	bat_percent=$(cat /sys/class/power_supply/BAT0/capacity)%
 	wifi_icon=$(echo -e '\uf1eb')
+	wifi_name=$(nmcli device status | grep connected | grep wifi | awk '{print $4}')
+	date_time=$(date | awk '{print $1" "$2" "$3" "$4}')
 
-	xsetroot -name " $wifi_icon  | $bat_icon  $bat_percent"
+	xsetroot -name " $wifi_icon  $wifi_name  < $bat_icon  $bat_percent < $date_time"
 
 	sleep 2
 done
