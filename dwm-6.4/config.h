4444fac9710c913e731e858255eaf81a5698141c
arthurshmidt <arthur@arthurshmidt.com>
2024-02-13 21:56:41 -0500

    Updated status bar and gaps

--- dwm-6.4/config.h
+++ dwm-6.4/config.h
@@ -2,12 +2,12 @@
 
 /* appearance */
 static const unsigned int borderpx  = 5;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 10;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int vertpad            = 15;       /* vertical padding of bar */
-static const int sidepad            = 5;       /* horizontal padding of bar */
+static const int sidepad            = 10;       /* horizontal padding of bar */
 static const char *fonts[]          = { "Terminess Nerd Font:size=10" };
 static const char dmenufont[]       = "Terminess Nerd Font:size=10";
 static const char col_gray1[]       = "#222222";
