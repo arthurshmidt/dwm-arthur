439dc4517bbf84e9c715af57b5d103d1395f05bb
arthurshmidt <arthur@arthurshmidt.com>
2024-02-21 22:59:57 -0500

    updated status bar script for cleaner date & time

--- dwm-6.4/scripts/bar-update.py
+++ dwm-6.4/scripts/bar-update.py
@@ -4,6 +4,7 @@
 # import statements
 import os
 import time
+from datetime import datetime
 import subprocess
 
 # Flags
@@ -14,7 +15,7 @@ space = " "
 space_arrow = " < "
 
 # time commands 
-date_icon = ""
+date_icon = "\ue641"
 date_time = "$(date | awk '{print $1 $2 $3 $4}')" 
 
 # Battery commands
@@ -26,11 +27,21 @@ wifi_icon = "\uf1eb"
 wifi_command = "$(nmcli device status | grep connected | grep wifi | awk '{print $4}')"
 
 # Combined commands
-command = ("xsetroot -name " + '"' + 
-    space + wifi_icon + 2*space + wifi_command + 
-    space_arrow + bat_icon + 2*space + bat_command + 
-    space_arrow + date_time +'"'
-)
+# command = ("xsetroot -name " + '"' + 
+#     space + wifi_icon + 2*space + wifi_command + 
+#     space_arrow + bat_icon + 2*space + bat_command + 
+#     space_arrow + date_icon + 2*space + date_time +'"'
+# )
+
+def get_command():
+    current_dt_time = datetime.now()
+    dt_string = current_dt_time.strftime("%d/%m/%Y %H:%M:%S")
+    command = ("xsetroot -name " + '"' + 
+        space + wifi_icon + 2*space + wifi_command + 
+        space_arrow + bat_icon + 2*space + bat_command + 
+        space_arrow + date_icon + 2*space + dt_string +'"'
+    )
+    return command
 
 # update loop and test if dwm closed out
 # intended to kill processes that were started specifically 
@@ -39,7 +50,7 @@ while is_running:
     #print(command)
     try:
         dwm_pid = subprocess.check_output(['pgrep','dwm'])
-        os.system(command)
+        os.system(get_command())
         is_running = True
     except:
         is_running = False
