590a44e1088de9270d16e009e3055a481fd16afd
arthurshmidt <arthur@arthurshmidt.com>
2024-02-21 21:57:56 -0500

    Updated dependencies for surf and got working

--- dwm-6.4/config.def.h
+++ dwm-6.4/config.def.h
@@ -32,7 +32,7 @@ static const Rule rules[] = {
 	/* class      instance    title       tags mask     isfloating   monitor */
 	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
 	{ "Firefox",  NULL,       NULL,       1 << 7,       0,           -1 },
-	{ "Chrome",   NULL,       NULL,       1,            0,           -1 },
+	{ "google-chrome",   NULL,       NULL,       1 << 2,            0,           -1 },
 };
 
 /* layout(s) */
