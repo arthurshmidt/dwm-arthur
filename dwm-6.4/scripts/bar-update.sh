f7c60ba627f1c943badaef027fb47991f1676c40
arthurshmidt <arthur@arthurshmidt.com>
2026-02-15 23:28:32 -0500

    Updated xinit

--- dwm-6.4/scripts/bar-update.sh
+++ dwm-6.4/scripts/bar-update.sh
@@ -3,9 +3,9 @@
 # Update loop
 while true; do
 	# Battery life
-	# xsetroot -name "$(echo -e '\uf240')  $(cat /sys/class/power_supply/BAT0/capacity)%"
+	# xsetroot -name "$(echo -e '\uf240')  $(cat /sys/class/power_supply/BAT1/capacity)%"
 	bat_icon=$(echo -e '\uf240')
-	bat_percent=$(cat /sys/class/power_supply/BAT0/capacity)%
+	bat_percent=$(cat /sys/class/power_supply/BAT1/capacity)%
 	wifi_icon=$(echo -e '\uf1eb')
 	wifi_name=$(nmcli device status | grep connected | grep wifi | awk '{print $4}')
 	date_time=$(date | awk '{print $1" "$2" "$3" "$4}')
